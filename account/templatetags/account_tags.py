a0a9ab6ded6e531a2bded68d1ebf1d2b5425da66
Shane <shane@shanerice.me>
2018-01-06 22:22:44 -0800

    Updated account home

---
+++ account/templatetags/account_tags.py
@@ -0,0 +1,10 @@
+import logging
+from django import template
+
+logger = logging.getLogger('app')
+register = template.Library()
+
+
+@register.filter(name='user_display')
+def user_display(value):
+    return '{}***************************'.format(str(value)[:6])
